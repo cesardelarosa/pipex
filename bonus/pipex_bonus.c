7deae43aa8da92856c6a83d93cd165bf439ba0c8
cde-la-r <cde-la-r@c1r4s1.42madrid.com>
2025-02-20 11:53:07 +0100

    nicer

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: cde-la-r <cde-la-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 00:17:00 by cde-la-r          #+#    #+#             */
-/*   Updated: 2025/02/20 11:35:21 by cde-la-r         ###   ########.fr       */
+/*   Updated: 2025/02/20 11:52:10 by cde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,38 +21,6 @@ t_pipex	*init_pipex(void)
 		ft_handle_errors("pipex", "malloc failed", NULL, 1);
 	ft_bzero(pipex, sizeof(t_pipex));
 	return (pipex);
-}
-
-void	free_pipex(t_pipex *pipex)
-{
-	if (pipex->cmds)
-	{
-		free(pipex->cmds);
-		pipex->cmds = NULL;
-	}
-	if (pipex->infile_fd >= 0)
-	{
-		close(pipex->infile_fd);
-		pipex->infile_fd = -1;
-	}
-	if (pipex->outfile_fd >= 0)
-	{
-		close(pipex->outfile_fd);
-		pipex->outfile_fd = -1;
-	}
-	if (pipex->pid_arr)
-	{
-		free(pipex->pid_arr);
-		pipex->pid_arr = NULL;
-	}
-	free(pipex);
-}
-
-void	free_and_exit(t_pipex *pipex, char *msg, char *cmd, int exit_code)
-{
-	if (pipex)
-		free_pipex(pipex);
-	ft_handle_errors("pipex", msg, cmd, exit_code);
 }
 
 int	main(int argc, char **argv, char **envp)
