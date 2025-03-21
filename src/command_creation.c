bfe7dd1180100b628951a80753c59e40477689a4
cde-la-r <cde-la-r@c1r4s1.42madrid.com>
2025-03-21 22:35:27 +0100

    force add src

---
+++ src/command_creation.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   command_creation.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-la-r <code@cesardelarosa.xyz>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 12:14:56 by cde-la-r          #+#    #+#             */
+/*   Updated: 2025/03/10 12:14:57 by cde-la-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "struct_creation.h"
+#include <stdlib.h>
+
+t_redir	*redir_create(t_redir_type type, char *file, t_command *parent_cmd)
+{
+	t_redir	*r;
+
+	r = ft_calloc(1, sizeof(t_redir));
+	if (!r)
+		return (NULL);
+	r->type = type;
+	r->file = ft_strdup(file);
+	if (!r->file)
+	{
+		free(r);
+		return (NULL);
+	}
+	r->cmd = parent_cmd;
+	return (r);
+}
+
+void	redir_destroy(void *content)
+{
+	t_redir	*r;
+
+	r = (t_redir *)content;
+	free(r->file);
+	free(r);
+}
+
+t_command	*command_create(char *cmd_str)
+{
+	t_command	*cmd;
+
+	cmd = ft_calloc(1, sizeof(t_command));
+	if (!cmd)
+		return (NULL);
+	cmd->argv = parse_args(cmd_str);
+	if (!cmd->argv)
+	{
+		free(cmd);
+		return (NULL);
+	}
+	cmd->redirs = NULL;
+	cmd->p = NULL;
+	return (cmd);
+}
+
+void	command_destroy(void *content)
+{
+	t_command	*cmd;
+
+	cmd = (t_command *)content;
+	ft_free_split(cmd->argv);
+	ft_lstclear(&cmd->redirs, redir_destroy);
+	free(cmd);
+}
+
+int	command_add_redir(t_command *cmd, t_redir_type type, char *file)
+{
+	t_redir	*r;
+	t_list	*node;
+
+	if (!cmd)
+		return (0);
+	r = redir_create(type, file, cmd);
+	if (!r)
+		return (0);
+	node = ft_lstnew(r);
+	if (!node)
+	{
+		redir_destroy(r);
+		return (0);
+	}
+	ft_lstadd_back(&cmd->redirs, node);
+	return (1);
+}
---
+++ src/pipeline_creation.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipeline_creation.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-la-r <code@cesardelarosa.xyz>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 12:16:25 by cde-la-r          #+#    #+#             */
+/*   Updated: 2025/03/10 12:16:26 by cde-la-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "struct_creation.h"
+
+t_pipeline	*pipeline_create(void)
+{
+	t_pipeline	*pipeline;
+
+	pipeline = ft_calloc(1, sizeof(t_pipeline));
+	return (pipeline);
+}
+
+void	pipeline_destroy(t_pipeline *pipeline)
+{
+	unsigned int	i;
+
+	if (pipeline->pipes)
+	{
+		i = 0;
+		while (i + 1 < pipeline->cmd_count)
+		{
+			free(pipeline->pipes[i]);
+			i++;
+		}
+		free(pipeline->pipes);
+	}
+	free(pipeline->pids);
+	ft_lstclear(&pipeline->commands, command_destroy);
+	free(pipeline);
+}
+
+int	pipeline_add_command(t_pipeline *p, t_command *cmd)
+{
+	t_list	*node;
+
+	node = ft_lstnew(cmd);
+	if (!node)
+		return (0);
+	ft_lstadd_back(&p->commands, node);
+	p->cmd_count++;
+	cmd->p = p;
+	return (1);
+}
